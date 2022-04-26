aa141f620c8a664eef7068d539153d399a978aa0
N3ther <thenetherreactor@gmail.com>
2022-04-26 08:23:04 -0400

    more maven stuff
    also fixed /ʋ/

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"net.oijon.algonquin","l":"App"},{"p":"net.oijon.algonquin","l":"AppTest"}];updateSearchResults();
