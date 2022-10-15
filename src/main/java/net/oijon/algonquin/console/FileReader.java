cb3cb64847645373a9a69d01f93a82ba249cb124
N3ther <alex@consus.fun>
2022-10-14 22:11:19 -0400

    work on .alg files

--- src/main/java/net/oijon/algonquin/console/FileReader.java
+++ src/main/java/net/oijon/algonquin/console/FileReader.java
@@ -1,5 +1,24 @@
 package net.oijon.algonquin.console;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class FileReader {
+	
+	public static void readFile(File file) {
+		try {
+			Scanner scanner = new Scanner(file);
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				String[] formatted = Console.parse(line, false);
+				
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 }
